efe0f6c85476d1b715caf3ff8cb90fab648a2a61
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-04-21 12:35:45 -0400

    home page

--- src/js/views/home-tesis.js
+++ src/js/views/home-tesis.js
@@ -4,13 +4,16 @@ import dnaImage from "../../img/dna-genetic-algorithm.jpg";
 import "../../styles/home-tesis.scss";
 
 export const HomeTesis = () => (
-	<div className="text-center mt-5">
-		<h1>Optimización de Estructuras con Algoritmos Genéticos!</h1>
+	<div className="text-center mt-6 title">
+		<h1 className="title">
+			Optimización de las Edificaciones de Acero con arriostramientos laterales en un sentido, condicionada a las
+			deriva de piso mediante la aplicación de Algoritmos Genéticos
+		</h1>
 		<p>
 			<img src={dnaImage} />
 		</p>
-		<button className="btn">
-			<Link to="/demo">If you see this green button, bootstrap is working</Link>
+		<button className="btnHome">
+			<Link to="/demo">Iniciar</Link>
 		</button>
 	</div>
 );
