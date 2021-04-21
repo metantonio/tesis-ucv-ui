8314bc1912dbcc566dd7ab5dc3ba4f63bd4f47fa
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-04-21 14:29:16 -0400

    cambios ui

--- src/js/views/stucture.js
+++ src/js/views/stucture.js
@@ -4,16 +4,22 @@ import { Link } from "react-router-dom";
 import "../../styles/structure.scss";
 
 export const Structure = () => (
-	<div className="text-center mt-6 title">
-		<h1 className="title">
-			Optimización de las Edificaciones de Acero con arriostramientos laterales en un sentido, condicionada a las
-			deriva de piso mediante la aplicación de Algoritmos Genéticos
-		</h1>
-		<p>
-			<h2 className="sub-title">Paso 1: Configuración de la Estructura</h2>
-		</p>
-		<button className="btnHome">
-			<Link to="/demo">Calcular</Link>
-		</button>
-	</div>
+	<>
+		<div className="text-center mt-6 title">
+			<h1 className="title">
+				Optimización de las Edificaciones de Acero con arriostramientos laterales en un sentido, condicionada a
+				las deriva de piso mediante la aplicación de Algoritmos Genéticos
+			</h1>
+		</div>
+		<div className="config-paso-1">
+			<p>
+				<h2 className="sub-title">Paso 1: Configuración de la Estructura</h2>
+			</p>
+			<p className="save-btn">
+				<button className="btnPaso text-center mt-12 title">
+					<span>Guardar Paso 1</span>
+				</button>
+			</p>
+		</div>
+	</>
 );
