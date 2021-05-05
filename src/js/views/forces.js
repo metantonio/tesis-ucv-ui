a1684aa4caf2f773386841652ff80d13370c6e30
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-05-05 12:44:35 -0400

    configuración página de casos de carga

--- src/js/views/forces.js
+++ src/js/views/forces.js
@@ -19,34 +19,41 @@ function Forces() {
 				</h1>
 			</div>
 			<div className="row justify-content">
+				<div className="col-md-12 justify-content">
+					<p>
+						<h2 className="sub-title">Paso 2: Configuración de las Cargas</h2>
+					</p>
+					<p>
+						<h4 className="sub-title 2">(COVENIN-MINDUR 2002-88)</h4>
+					</p>
+					<br />
+				</div>
 				<div className="col-md-6">
 					<div className="config-paso-1 justify-content">
 						<p>
-							<h2 className="sub-title">Paso 2: Configuración de las Cargas</h2>
-						</p>
-						<p>
-							<h4>Carga Viva (kgf/m)</h4>
+							<h4>Carga Viva Entrepiso(kgf/m2)</h4>
 							<input
 								className="no-columnas"
 								type="number"
-								placeholder="2"
+								placeholder="250.00"
 								id="no-columnas"
 								name="no-columnas"
-								min="2"
-								max="5"
+								min="1.00"
+								step="0.01"
 								//onChange={e => actions.setNoColumnas(e.target.value)}
 							/>
 						</p>
 						<p>
-							<h4>Carga Permanente (kgf/m)</h4>
+							<h4>Carga Permanente LOSA Entrepiso(kgf/m2)</h4>
 							<input
 								className="no-columnas"
 								type="number"
-								placeholder="1"
+								placeholder="800.00"
 								id="no-columnas"
 								name="no-columnas"
-								min="1"
-								max="8"
+								min="1.00"
+								step="0.01"
+
 								//onChange={e => actions.setNoPisos(e.target.value)}
 							/>
 						</p>
@@ -55,29 +62,76 @@ function Forces() {
 							<input
 								className="no-columnas"
 								type="number"
-								placeholder="1"
+								placeholder="0.00"
 								id="no-columnas"
 								name="no-columnas"
-								min="1.00"
+								min="0.00"
 								step="0.01"
-								max="6.00"
 								//onChange={e => actions.setEntrePiso(e.target.value)}
 							/>
 						</p>
 					</div>
 				</div>
-				<p>
-					<button className="btnPaso text-center mt-12 title">
-						<span>Calculo de Casos de Carga</span>
-					</button>
-				</p>
-				<p>
-					<button className="btnPaso2 text-center mt-12 title">
-						<Link to="/">
-							<span>Ir al Paso 3</span>
-						</Link>
-					</button>
-				</p>
+				<div className="col-md-6">
+					<div className="config-paso-1 justify-content">
+						<p>
+							<h4>Carga Viva Techo(kgf/m2)</h4>
+							<input
+								className="no-columnas"
+								type="number"
+								placeholder="100.00"
+								id="no-columnas"
+								name="no-columnas"
+								min="40.00"
+								step="0.01"
+
+								//onChange={e => actions.setNoColumnas(e.target.value)}
+							/>
+						</p>
+						<p>
+							<h4>Carga Permanente Techo(kgf/m2)</h4>
+							<input
+								className="no-columnas"
+								type="number"
+								placeholder="150.00"
+								id="no-columnas"
+								name="no-columnas"
+								min="1.00"
+								step="0.01"
+
+								//onChange={e => actions.setNoPisos(e.target.value)}
+							/>
+						</p>
+						<p>
+							<h4>Viento (kgf/m2)</h4>
+							<input
+								className="no-columnas"
+								type="number"
+								placeholder="0.00"
+								id="no-columnas"
+								name="no-columnas"
+								min="0.00"
+								step="0.01"
+
+								//onChange={e => actions.setEntrePiso(e.target.value)}
+							/>
+						</p>
+					</div>
+				</div>
+				<div className="btn sub-title col-md-12">
+					<p>
+						<button className="btnPaso text-center mt-12 title">
+							<span>Guardar configuración de Cargas</span>
+						</button>
+					</p>
+					<p>
+						<button className="btnPaso2 text-center mt-12 title">
+							<Link to="/">
+								<span>Ir al Paso 3</span>
+							</Link>
+						</button>
+					</p>
+				</div>
 			</div>
 		</React.Fragment>
 	);
